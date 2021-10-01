12f90551e53438a19349915834cbb2168c81a384
Tiara Brown <tiaradominique@gmail.com>
2021-10-01 15:35:08 -0400

    I added a y,n option to the guessing game

--- js/app.js
+++ js/app.js
@@ -1,58 +1,59 @@
-'use strict';
+"use strict";
 
-let username = prompt('What is your name?')
+let username = prompt("What is your name?");
 
-alert ('Hello ' + username + ' , welcome to my About Me page!');
+alert("Hello " + username + " , welcome to my About Me page!");
 
-let answerSports = prompt('Does Tiara like sports? Yes or No').toLowerCase();
+let answerSports = prompt("Does Tiara like sports? Yes or No").toLowerCase();
 
-
-if (answerSports === 'no'){
-  alert('You answered correctly!');
+if (answerSports === "no" || answerSports === "n") {
+  alert("You answered correctly!");
   // console.log('You answered correctly!');
 } else {
-  alert('Sorry, you are wrong.');
+  alert("Sorry, you are wrong.");
   // console.log('You answered correctly!');
 }
 
-let answerMilk = prompt('Can Tiara drink milk? Yes or No').toLowerCase();
+let answerMilk = prompt("Can Tiara drink milk? Yes or No").toLowerCase();
 
-if (answerMilk === 'no'){
-  alert('You answered correctly!');
+if (answerMilk === "no" || answerMilk === "n") {
+  alert("You answered correctly!");
   // console.log('You answered correctly!');
 } else {
-  alert('Sorry, you are wrong.');
+  alert("Sorry, you are wrong.");
   // console.log('You answered correctly!');
 }
 
-let answerPet = prompt('Does Tiara have a dog? Yes or No').toLowerCase();
+let answerPet = prompt("Does Tiara have a dog? Yes or No").toLowerCase();
 
-if (answerPet === 'yes'){
-  alert('You answered correctly!');
+if (answerPet === "yes" || answerPet === "y") {
+  alert("You answered correctly!");
   // console.log('You answered correctly!');
 } else {
-  alert('Sorry, you are wrong.');
+  alert("Sorry, you are wrong.");
   // console.log('You answered correctly!');
 }
 
-let answerBoyfriend = prompt('Does Tiara have a boyfriend? Yes or No').toLowerCase();
+let answerBoyfriend = prompt(
+  "Does Tiara have a boyfriend? Yes or No"
+).toLowerCase();
 
-if (answerBoyfriend === 'yes'){
-  alert('You answered correctly!');
+if (answerBoyfriend === "yes" || answerBoyfriend === "y") {
+  alert("You answered correctly!");
   // console.log('You answered correctly!');
 } else {
-  alert('Sorry, you are wrong.');
+  alert("Sorry, you are wrong.");
   // console.log('You answered correctly!');
 }
 
-let answerHair = prompt('Does Tiara have short hair? Yes or No').toLowerCase();
+let answerHair = prompt("Does Tiara have short hair? Yes or No").toLowerCase();
 
-if (answerHair === 'yes'){
-  alert('You answered correctly!');
+if (answerHair === "yes" || answerHair === "y") {
+  alert("You answered correctly!");
   // console.log('You answered correctly!');
 } else {
-  alert('Sorry, you are wrong.');
+  alert("Sorry, you are wrong.");
   // console.log('You answered correctly!');
 }
 
-alert("Thank you, " + username + ", for answering my questions.")
+alert("Thank you, " + username + ", for answering my questions.");
