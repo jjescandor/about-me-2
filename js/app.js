d704eb9a991240e5e50049fec856f369c095ce67
JJ Escandor <jj.p.escandor@gmail.com>
2021-10-06 21:50:48 -0400

    First question, function created

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 "use strict";
 
 let score = 0;
-
 let username = prompt("What is your name?");
+let arrayOfAnswersOne = ['no', 'no', 'yes', 'yes', 'yes']
+let arrayOfAnswersTwo = ['n', 'n', 'y', 'y', 'y']
 
 alert("Hello " + username + " , welcome to my About Me page!");
 
-let answerSports = prompt("Does Tiara like sports? Yes or No").toLowerCase();
+function checkAnswer(userAnswer, storedAnswerOne, storedAnswerTwo) {
+  if (userAnswer === storedAnswerOne || userAnswer === storedAnswerTwo) {
+    alert("You answered correctly!");
+    score++;
+    // console.log('You answered correctly!');
+  } else {
+    alert("Sorry, you are wrong.");
+    // console.log('You answered correctly!');
+  }
+};
 
-if (answerSports === "no" || answerSports === "n") {
-  alert("You answered correctly!");
-  score++;
-  // console.log('You answered correctly!');
-} else {
-  alert("Sorry, you are wrong.");
-  // console.log('You answered correctly!');
-}
+function questionOne() {
+  let answerSports = prompt("Does Tiara like sports? Yes or No").toLowerCase();
+  checkAnswer(answerSports, arrayOfAnswersOne[0], arrayOfAnswersTwo[0]);
+};
+questionOne();
 
 let answerMilk = prompt("Can Tiara drink milk? Yes or No").toLowerCase();
 
@@ -70,27 +77,27 @@ let attempts = 0;
 
 //let numberGuesser = prompt('Guess a number between 1-20');
 
- let correctAnswer = 8;
+let correctAnswer = 8;
 
 
-for(let i = 4; i >= attempts; i-- ) {
-    let correctAnswer = 8;
-    let numberGuesser = parseInt(prompt('Guess a number between 1-20', ''));
+for (let i = 4; i >= attempts; i--) {
+  let correctAnswer = 8;
+  let numberGuesser = parseInt(prompt('Guess a number between 1-20', ''));
 
-    if(numberGuesser === correctAnswer) {
-        alert('You are correct.');
-        score++;
-        break;
-    } else if (numberGuesser < correctAnswer) {
-        alert(`Your guess is too low. You have ${(i-1)} attempts remaining.`);
-    } else if(numberGuesser > correctAnswer) {
-        alert(`Your guess is too high. You have ${(i)} attempts remaining.`);
-    } 
-    
-    if ((i) === 1){
-      alert(`The correct answer was ${correctAnswer}.`);
-      break;
-    }
+  if (numberGuesser === correctAnswer) {
+    alert('You are correct.');
+    score++;
+    break;
+  } else if (numberGuesser < correctAnswer) {
+    alert(`Your guess is too low. You have ${(i - 1)} attempts remaining.`);
+  } else if (numberGuesser > correctAnswer) {
+    alert(`Your guess is too high. You have ${(i)} attempts remaining.`);
+  }
+
+  if ((i) === 1) {
+    alert(`The correct answer was ${correctAnswer}.`);
+    break;
+  }
 
 }
 
@@ -105,7 +112,7 @@ for(let i = 4; i >= attempts; i-- ) {
 
 // have your answers in an array for your 7th question that have six attempts with multiple correct answer. You can not use a function yet
 
-let petsName = ['chance','weiner','stinky boy'];
+let petsName = ['chance', 'weiner', 'stinky boy'];
 
 
 let guesses = 0;
@@ -114,7 +121,7 @@ for (let k = 6; k > guesses; k--) {
   let userResponse = prompt('What is my dogs name? (there are multiple correct answers)').toLowerCase();
   for (let j = 0; j < petsName.length; j++) {
     if (userResponse.toLowerCase() === petsName[j]) {
-      alert('That is correct! '+ username);
+      alert('That is correct! ' + username);
       correct = true;
       score++;
     }
